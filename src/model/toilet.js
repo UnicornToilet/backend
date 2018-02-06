2f1a99579fda5604b5775166694a933814eb9804
unknown <joel.epstein2@gmail.com>
2018-02-05 16:50:51 -0800

    test

--- src/model/toilet.js
+++ src/model/toilet.js
@@ -1,61 +1,49 @@
-// 'use strict'
+import createError from 'http-errors'
+import * as util from '../lib/util.js'
+import Mongoose, {Schema} from 'mongoose'
 
-// // DEPENDECIES
-// import * as bcrypt from 'bcrypt'
-// import {randomBytes} from 'crypto'
-// import * as jwt from 'jsonwebtoken'
-// import createError from 'http-errors'
-// import {promisify} from '../lib/util.js'
-// import Mongoose, {Schema} from 'mongoose'
+const toiletScheama = new Schema({
+  toilet: {
+    overallQuality: Number,
+    tpQuality: Number,
+    genderNeutral: Boolean,
+    occupancy: Boolean,
+    soap: String,
+    dryingMethod: String,
+    babyChanging: String,
+    },
+})
 
-// // SCHEMA
-// const userSchema =  new Schema({
-//   email: {type: String, required: true, unique: true},
-//   username: {type: String, required: true, unique: true},
-//   passwordHash: {type: String, required: true},
-//   randomHash: {type: String,  unique: true, default: ''},
-//   profile: {type: Schema.Types.ObjectId},
-// })
+const Toilet = Mongoose.model('toilet', toiletScheama)
 
-// // INSTANCE METHODS
-// userSchema.methods.passwordCompare = function(password){
-//   return bcrypt.compare(password, this.passwordHash)
-//   .then(success => {
-//     if (!success)
-//       throw createError(401, 'AUTH ERROR: wrong password')
-//     return this
-//   })
-// }
+Toilet.create = function (toilet) {
+    let data = Object.assign({}, toilet) 
+    return new Toilet(data).save()
+  }
 
-// userSchema.methods.tokenCreate  = function(){
-//   this.randomHash = randomBytes(32).toString('base64')
-//   return this.save()
-//   .then(user => {
-//     return jwt.sign({randomHash: this.randomHash}, process.env.SECRET)
-//   })
-//   .then(token => {
-//     return token
-//   })
-// }
+  return new Profile({
+    owner: req.user._id,
+    username: req.user.username, 
+    email: req.user.email,
+    bio: req.body.bio,
+  })
+  .save()
+  .then(profile => {
+    req.user.profile = profile._id
+    return req.user.save()
+    .then(() => profile)
+  })
+}
 
-// // MODEL
-// const User = Mongoose.model('user', userSchema)
+Profile.fetchAll = util.pagerCreate(Profile)
 
-// // STATIC METHODS
-// User.create = function (user) {
-//   if(!user.password || !user.email || !user.username)
-//     return Promise.reject(
-//       createError(400, 'VALIDATION ERROR: missing username email or password '))
+Profile.fetchOne = function(req){
+  return Profile.findById(req.params.id)
+  .then(profile => {
+    if(!profile)
+      throw createError(404, 'NOT FOUND ERROR: profile not found') 
+    return profile
+  })
+}
 
-//   let {password} = user
-//   user = Object.assign({}, user, {password: undefined})
-
-//   return bcrypt.hash(password, 1)
-//   .then(passwordHash => {
-//     let data = Object.assign({}, user, {passwordHash}) 
-//     return new User(data).save()
-//   })
-// }
-
-// // INTERFACE
-// export default User
+export default Toilet
