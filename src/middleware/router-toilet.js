792f2ae9cdb2bd05063ecdcc1fa58cc693edb7a8
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-02-08 20:42:50 -0800

    filter

--- src/middleware/router-toilet.js
+++ src/middleware/router-toilet.js
@@ -13,28 +13,28 @@ export default new Router()
 
   .post('/toilet', parserBody, (req, res, next) => {
     const { overallQuality, tpQuality, occupancy, soap, dryingMethod, babyChanging } = req.body;
-    const query = Toilet.find()
-      if (overallQuality) {
-        query.where('overallQuality').gte(overallQuality)
-      };
-      if (tpQuality) {
-        query.where('tpQuality').gte(tpQuality)
-      };
-      if (occupancy) {
-        query.where('occupancy').equals(occupancy)
-      };
-      if (soap) {
-        query.where('soap').equals(soap)
-      };
-      if (dryingMethod) {
-        query.where('dryingMethod').equals(dryingMethod)
-      };
-      if (babyChanging) {
-        query.where('babyChanging').equals(babyChanging)
-      };
-      query.exec((err, toilets) => {
-        res.send(toilets)
-      })
+    const query = Toilet.find();
+    if (overallQuality) {
+      query.where('overallQuality').gte(overallQuality);
+    }
+    if (tpQuality) {
+      query.where('tpQuality').gte(tpQuality);
+    }
+    if (occupancy) {
+      query.where('occupancy').equals(occupancy);
+    }
+    if (soap) {
+      query.where('soap').equals(soap);
+    }
+    if (dryingMethod) {
+      query.where('dryingMethod').equals(dryingMethod);
+    }
+    if (babyChanging) {
+      query.where('babyChanging').equals(babyChanging);
+    }
+    query.exec((err, toilets) => {
+      res.send(toilets);
+    });
   })
   
   .get('/toilet', (req, res, next) => {
