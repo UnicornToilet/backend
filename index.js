4510e8fdf6207425862428a5bca805d5c3ef2a37
Kyle Richey <richey.kyle.i@gmail.com>
2018-02-08 19:25:16 -0800

    index.js

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-// require('dotenv').config();
-// require('babel-register');
-// require('./src/main.js');
-
-let express = require('express');
-let morgan = require('morgan');
-
-let app = express();
-
-let PORT = process.env.PORT || 8080;
-
-app.use(morgan('common'));
-
-app.use(express.static(`${__dirname}/build`));
-app.use('*', (req,res) => res.send(`${__dirname}/build/index.html`));
-
-app.listen(PORT, () => {
-    console.log("Server up on port", PORT);
-});
+require('dotenv').config();
+require('babel-register');
+require('./src/main.js');
