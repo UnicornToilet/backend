e6e241b41bdb7905b7d5791a6dec302b33f14c6e
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-02-10 10:19:06 -0800

    very MVP

--- src/middleware/router-toilet.js
+++ src/middleware/router-toilet.js
@@ -14,7 +14,7 @@ export default new Router()
   })
 
   .post('/toilet', parserBody, (req, res, next) => {
-    const { overallQuality, tpQuality, occupancy, soap, drying, babyChanging } = req.body;
+    const { overallQuality, tpQuality, soap, drying} = req.body;
     const query = Toilet.find();
     if (overallQuality) {
       query.where('overallQuality').gte(overallQuality);
@@ -22,17 +22,11 @@ export default new Router()
     if (tpQuality) {
       query.where('tpQuality').gte(tpQuality);
     }
-    if (occupancy) {
-      query.where('occupancy').equals(occupancy);
-    }
     if (soap) {
       query.where('soap').equals(soap);
     }
     if (drying) {
       query.where('drying').equals(drying);
-    }
-    if (babyChanging) {
-      query.where('babyChanging').equals(babyChanging);
     }
     query.exec((err, toilets) => {
       res.send(toilets);
